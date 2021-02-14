279c1ce712be0d920d43a63a2bed2f9b1b9e0a34
Jake Heft <jakeheft@gmail.com>
2021-02-14 14:00:56 -0700

    Add migration to add category column to street_cafes table

--- street_cafes/db/schema.rb
+++ street_cafes/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_200550) do
+ActiveRecord::Schema.define(version: 2021_02_14_205306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_200550) do
     t.string "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
