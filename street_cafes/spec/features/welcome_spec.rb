27d46a98da23c0afed85925c7a15c78053a6a44a
Jake Heft <jakeheft@gmail.com>
2021-02-15 12:55:34 -0700

    Add expectations to test for categorizing cafes

--- street_cafes/spec/features/welcome_spec.rb
+++ street_cafes/spec/features/welcome_spec.rb
@@ -9,10 +9,11 @@ describe 'Welcome Page' do
 		cafe2 = create(:street_cafe, post_code: post_code1, num_chairs: 5)
 		cafe3 = create(:street_cafe, post_code: post_code1, num_chairs: 101)
 		cafe4 = create(:street_cafe, post_code: post_code1, num_chairs: 100)
-		cafe5 = create(:street_cafe, post_code: post_code2, num_chairs: 200)
-		cafe6 = create(:street_cafe, post_code: post_code2, num_chairs: 20)
-		cafe7 = create(:street_cafe, post_code: post_code2, num_chairs: 32)
-		cafe8 = create(:street_cafe, post_code: post_code3, num_chairs: 32)
+		cafe5 = create(:street_cafe, post_code: post_code2, num_chairs: 50)
+		cafe6 = create(:street_cafe, post_code: post_code2, num_chairs: 45)
+		cafe7 = create(:street_cafe, post_code: post_code2, num_chairs: 44)
+		cafe8 = create(:street_cafe, post_code: post_code2, num_chairs: 43)
+		cafe9 = create(:street_cafe, post_code: post_code3, num_chairs: 32)
 
 		expect(cafe1.category).to eq(nil)
 		expect(cafe2.category).to eq(nil)
@@ -22,11 +23,21 @@ describe 'Welcome Page' do
 		expect(cafe6.category).to eq(nil)
 		expect(cafe7.category).to eq(nil)
 		expect(cafe8.category).to eq(nil)
+		expect(cafe9.category).to eq(nil)
 
 		visit root_path
 
 		click_button 'Categorize Cafes'
 
 		expect(current_path).to eq('/categories')
+		expect(cafe1.category).to eq('ls1 medium')
+		expect(cafe2.category).to eq('ls1 small')
+		expect(cafe3.category).to eq('ls1 large')
+		expect(cafe4.category).to eq('ls1 large')
+		expect(cafe5.category).to eq('ls2 large')
+		expect(cafe6.category).to eq('ls2 large')
+		expect(cafe7.category).to eq('ls2 small')
+		expect(cafe8.category).to eq('ls2 small')
+		expect(cafe9.category).to eq('other')
 	end
 end
