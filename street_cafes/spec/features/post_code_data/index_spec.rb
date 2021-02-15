e4f0e5e885bfcbdef7435d5293bb3bee32c7c342
Jake Heft <jakeheft@gmail.com>
2021-02-15 10:54:04 -0700

    Update test for post code data

--- street_cafes/spec/features/post_code_data/index_spec.rb
+++ street_cafes/spec/features/post_code_data/index_spec.rb
@@ -19,13 +19,15 @@ describe 'Post Code Index' do
 		expect(page).to have_content('total_chairs')
 		expect(page).to have_content('chairs_pct')
 		expect(page).to have_content('place_with_max_chairs')
-		
-		within("##{post_code1}") do
-			
-		end
-
-		within("##{post_code2}") do
-			
-		end
+		expect(page).to have_content('LS1 5EL')
+		expect(page).to have_content(4)
+		expect(page).to have_content(175)
+		expect(page).to have_content(40.98)
+		expect(page).to have_content(cafe3.name)
+		expect(page).to have_content('LS1 3AD')
+		expect(page).to have_content(3)
+		expect(page).to have_content(252)
+		expect(page).to have_content(59.02)
+		expect(page).to have_content(cafe5.name)		
 	end
 end
