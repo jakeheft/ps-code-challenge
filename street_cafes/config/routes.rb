d99eca63fa007f871741093edb8a32428fcafb91
Jake Heft <jakeheft@gmail.com>
2021-02-15 12:21:10 -0700

    Add routes to categories resource

--- street_cafes/config/routes.rb
+++ street_cafes/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+
   resources :post_code_data, only: %i[index]
+
+  resources :categories, only: %i[index create]
 end
