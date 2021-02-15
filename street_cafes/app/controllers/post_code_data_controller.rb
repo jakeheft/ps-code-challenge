ba5b22e4c20e5c1c0cbabf288bf23116dc3719f2
Jake Heft <jakeheft@gmail.com>
2021-02-15 10:54:43 -0700

    Update index action to create post codes

--- street_cafes/app/controllers/post_code_data_controller.rb
+++ street_cafes/app/controllers/post_code_data_controller.rb
@@ -1,5 +1,6 @@
 class PostCodeDataController < ApplicationController
 	def index
-		@post_codes = StreetCafe.post_code_data
+		post_codes = StreetCafe.distinct.pluck(:post_code)
+		@post_codes = post_codes.map { |code| PostCode.new(code) }
 	end
 end
