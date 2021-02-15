aad6e1827365be5bf07e50dac536dec8678ff3a5
Jake Heft <jakeheft@gmail.com>
2021-02-15 10:53:29 -0700

    Add chairs pct and place with max chairs methods to post code poro

--- street_cafes/app/poros/post_code.rb
+++ street_cafes/app/poros/post_code.rb
@@ -5,4 +5,12 @@ class PostCode
 		@total_places = StreetCafe.where(post_code: code).group(:post_code).count[code]
 		@total_chairs = StreetCafe.where(post_code: code).sum(:num_chairs)
 	end
+
+	def chairs_pct
+		(total_chairs.to_f / StreetCafe.sum(:num_chairs)) * 100
+	end
+
+	def place_with_max_chairs
+		StreetCafe.where(post_code: code).order(num_chairs: :desc).pluck(:name).first
+	end
 end
