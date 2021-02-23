5d55399bac8f22912eb9efcb54e7bf2cdb26f647
Jake Heft <jakeheft@gmail.com>
2021-02-23 12:12:00 -0700

    Convert activerecord to sql queries

--- street_cafes/app/controllers/post_code_data_controller.rb
+++ street_cafes/app/controllers/post_code_data_controller.rb
@@ -1,6 +1,7 @@
 class PostCodeDataController < ApplicationController
 	def index
-		post_codes = StreetCafe.distinct.pluck(:post_code)
+		post_codes = StreetCafe.find_by_sql("SELECT DISTINCT street_cafes.post_code FROM street_cafes")
+		post_codes = post_codes.map { |code| code.post_code }
 		@post_codes = post_codes.map { |code| PostCode.new(code) }
 	end
 end
